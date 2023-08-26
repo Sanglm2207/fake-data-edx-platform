b971d9a1fcb86fdf0d28313fc3ae91ecff9e4c8e
SangLM-Dev <sanglm2207@gmail.com>
2023-08-26 23:32:28 +0700

    add cors

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const generateData = require('./generate-data');
 const asmList = require('./asm-list');
 
 const app = express();
 const port = 3000;
 
+// Use the cors middleware
+app.use(cors());
+
 app.get('/generate-data', (req, res) => {
     const data = generateData();
-    res.json(data);
+    res.json(data); 
 });
 
 app.get('/asmList', (req, res) => {
