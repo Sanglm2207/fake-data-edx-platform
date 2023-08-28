32529dcb1f6d65b4fbff103f5e611e0fee99e55e
SangLM-Dev <sanglm2207@gmail.com>
2023-08-28 15:55:26 +0700

    push

--- asm-list.js
+++ asm-list.js
@@ -1,60 +1,44 @@
 var faker = require('faker');
 
 function asmList() {
-    var data = [];
-    var statusValues = [null, 'File Submitted', 'Submitted', 'Assigned', 'Passed', 'Not Passed'];
+    var data = {
+        "Status_res": "",
+        "expired_time": "",
+        "assessment_element_list": []
+    };
+
+    var Status_res1 = ['Input Matched', 'Cannot Find'];
     var assignmentElementTitles = ['Assignment 1', 'Assignment 2', 'Assignment 3', 'Assignment 4', 'Assignment 5', 'Assignment 6'];
     var otherElementTitles = ['Lab', 'Final Exam'];
-    var Status_res1 = ['Input Matched', 'Cannot Find'];
-    
+
     for (var id = 0; id < 10; id++) {
-        let course_code = faker.hacker.phrase();
         let asssessment_element_title = faker.random.arrayElement([...assignmentElementTitles, ...otherElementTitles]);
-        let status = faker.random.arrayElement(statusValues);
-        let Status_res = '';
-        
-        // Check Status and set corresponding Status_res
-        if (status === 'NULL') {
-            Status_res = 'Input Matched';
-        } else if (status === 'File Submitted') {
-            Status_res = 'Cannot Find Email';
-        } else if (status === 'Submitted') {
-            Status_res = 'Cannot Find Instant';
-        } else {
-            Status_res = faker.random.arrayElement(Status_res1);
-        }
-
-        // Check if the selected assessment element title is an assignment from 1 to 6
-        if (assignmentElementTitles.includes(asssessment_element_title)) {
-            var asssessment_type_title = 'Assignment';
-        } else if (asssessment_element_title === 'Lab') {
-            var asssessment_type_title = 'Lab';
-        } else if (asssessment_element_title === 'Final Exam') {
-            var asssessment_type_title = 'Final Exam';
-        }
-
-        let mark = faker.random.number({ min: 0, max: 10 });
-        let review_times = faker.random.number(1);
-        let review_times_max = faker.random.number(1);
+        let status = faker.random.arrayElement(['File Submitted', 'Assigned']);
+        let Status_res = faker.random.arrayElement(Status_res1);
         let expired_time = faker.date.between("2018-01-01", "2018-07-31").toISOString().split("T")[0];
+        
+        let asssessment_type_title = '';
+        if (assignmentElementTitles.includes(asssessment_element_title)) {
+            asssessment_type_title = 'Assignment';
+        } else if (asssessment_element_title === 'Lab') {
+            asssessment_type_title = 'Lab';
+        } else if (asssessment_element_title === 'Final Exam') {
+            asssessment_type_title = 'Final Exam';
+        }
 
-        data.push({
-            "Status_res": Status_res,
-            "expired_time": expired_time,
-            "assessment_element_list": [
-                {
-                    // "asssessment_element_id": course_code,
-                    "asssessment_element_title": asssessment_element_title,
-                    "asssessment_type_title": asssessment_type_title,
-                    "option": ["1", "2"],
-                    "Required_File": true,
-                    "Required_Interview": true,
-                    "Status": status,
-                    "Mark": mark,
-                    "Review_times": review_times,
-                    "Review_times_max": review_times_max
-                }
-            ]
+        data.Status_res = Status_res;
+        data.expired_time = expired_time;
+
+        data.assessment_element_list.push({
+            "asssessment_element_title": asssessment_element_title,
+            "asssessment_type_title": asssessment_type_title,
+            "option": ["1", "2"],
+            "Required_File": true,
+            "Required_Interview": true,
+            "Status": status,
+            "Mark": faker.random.number({ min: 0, max: 10 }),
+            "Review_times": faker.random.number(1),
+            "Review_times_max": faker.random.number(1)
         });
     }
 
