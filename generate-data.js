8028943db0dfcf940d13d3375c72b0af68943e4e
SangLM-Dev <sanglm2207@gmail.com>
2023-10-02 10:52:21 +0700

    change link

--- generate-data.js
+++ generate-data.js
@@ -23,7 +23,7 @@ function generateData() {
             "date_register": date_register,
             "time_register": time_register, // Morning / Afternoon/ Evening
             "datetime_interview": datetime_interview,
-            "File_link": "https://example.com",
+            "File_link": "https://drive.google.com/file/d/0B-hTIM3aG_oJWnpxdGtXV2Z2YlE/view?usp=sharing&export=download",
             "Mentor_email": "mentorabc@funix.edu.vn",
             "Option_selected": "1",
             "Comment_Mentor": "Học viên làm bài tốt, hiểu bài",
